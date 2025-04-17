0a0dbb093d82db925a7b81a3813ed276dd02d1a2
Tendo33 <sjf1998112@gmail.com>
2025-04-17 13:32:22 +0800

    feat(frontend): add global styles and enhance App component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <RouterView />
+  <div class="app-container">
+    <RouterView />
+  </div>
 </template>
 
 <script setup lang="ts">
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
+import { useHead } from '@vueuse/head'
+
 useHead({
   title: '调酒师助手',
   meta: [
@@ -16,3 +20,11 @@ useHead({
   ],
 })
 </script>
+
+<style>
+.app-container {
+  min-height: 100vh;
+  width: 100%;
+  background-color: var(--el-bg-color-page);
+}
+</style>
